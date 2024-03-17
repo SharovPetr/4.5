6c6e01a908706805edb5bc0849ce7ce74b9d409f
Petr <pedro.shr@mail.ru>
2024-03-17 15:15:41 +0300

    edit contacts

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,8 +1,8 @@
 <h1>Контакты</h1>
 
-<p>Телефон: +7-953-565-3760</p>
+<p>Телефон: <a href="tel:+79535653760" class = "link">+7-953-565-3760</a></p>
 <p>Почта: <a href="mailto:pedro.shr@mail.ru" class = "link">pedro.shr@mail.ru</a></p>
-<p>VK: <a href="tel:https://vk.com/ip6655" class = "link">https://vk.com/ip6655</a></p>
+<p>VK: <a href="https://vk.com/ip6655" class = "link">https://vk.com/ip6655</a></p>
 
 <style>
 .link {
